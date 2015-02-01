54977592b136eb8a8807f26f1fa25840fb3db1ad
Johannes Schüth <git@jotschi.de>
2015-02-01 23:28:34 +0100

    mend

--- core/src/main/java/io/vertx/ext/graph/neo4j/Neo4VertxConfiguration.java
+++ core/src/main/java/io/vertx/ext/graph/neo4j/Neo4VertxConfiguration.java
@@ -4,6 +4,8 @@ import io.vertx.core.json.JsonObject;
 
 import java.io.File;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.neo4j.cluster.ClusterSettings;
 import org.neo4j.kernel.ha.HaSettings;
 import org.openpcf.neo4vertx.neo4j.Neo4jGraph;
@@ -18,25 +20,46 @@ import org.openpcf.neo4vertx.neo4j.Neo4jGraph;
 public class Neo4VertxConfiguration {
 
     public static final String MODE_KEY = "mode";
-    public static String PATH_KEY = "path";
-    public static String REST_URL_KEY = "restUrl";
-    public static String BASE_ADDR_KEY = "baseAddress";
-    public static final String WEB_SERVER_BIND_ADDRESS_KEY = "webServerBindAddress";
+    public static final String PATH_KEY = "path";
+    public static final String REST_URL_KEY = "rest_url";
+    public static final String BASE_ADDR_KEY = "base_address";
+    public static final String HA_INITIAL_HOSTS_KEY = "ha_initial_hosts";
+    public static final String HA_CLUSTER_SERVER_KEY = "ha_cluster_server";
+    public static final String HA_SLAVE_ONLY_KEY = "ha_slave_only";
+    public static final String HA_SERVER_KEY = "ha_server";
+    public static final String WEB_SERVER_BIND_ADDRESS_KEY = "webserver_bind_address";
+    public static final String HA_SERVER_ID_KEY = "ha_server_id";
 
     public final static String DEFAULT_WEBSERVER_BIND_ADDRESS = "localhost";
     public final static String DEFAULT_PATH = System.getProperty("user.dir") + File.separator + "db";
     public final static String DEFAULT_REST_URL = "http://localhost:7474/db/data/cypher";
     public final static String DEFAULT_BASE_ADDRESS = "neo4j-graph";
 
-    private String webServerBindAddress;
     private String mode;
     private String path;
+    
+    @JsonProperty(WEB_SERVER_BIND_ADDRESS_KEY)
+    private String webServerBindAddress;
+
+    @JsonProperty(REST_URL_KEY)
     private String restUrl;
+
+    @JsonProperty(BASE_ADDR_KEY)
     private String baseAddress;
+
+    @JsonProperty(HA_INITIAL_HOSTS_KEY)
     private String haInitialHosts;
+
+    @JsonProperty(HA_CLUSTER_SERVER_KEY)
     private String haClusterServer;
+
+    @JsonProperty(HA_SLAVE_ONLY_KEY)
     private boolean haSlaveOnly;
+
+    @JsonProperty(HA_SERVER_KEY)
     private String haServer;
+
+    @JsonProperty(HA_SERVER_ID_KEY)
     private String haServerId;
 
     public Neo4VertxConfiguration() {
@@ -49,11 +72,11 @@ public class Neo4VertxConfiguration {
         path = jsonConfig.getString(PATH_KEY, DEFAULT_PATH);
         restUrl = jsonConfig.getString(REST_URL_KEY, DEFAULT_REST_URL);
         baseAddress = jsonConfig.getString(BASE_ADDR_KEY, DEFAULT_BASE_ADDRESS);
-        haInitialHosts = jsonConfig.getString(ClusterSettings.initial_hosts.name(), null);
-        haClusterServer = jsonConfig.getString(ClusterSettings.cluster_server.name(), ClusterSettings.cluster_server.getDefaultValue());
-        haSlaveOnly = Boolean.valueOf(jsonConfig.getString(HaSettings.slave_only.name(), HaSettings.slave_only.getDefaultValue()));
-        haServer = jsonConfig.getString(HaSettings.ha_server.name(), HaSettings.ha_server.getDefaultValue());
-        haServerId = jsonConfig.getString(ClusterSettings.server_id.name(), null);
+        haInitialHosts = jsonConfig.getString(HA_INITIAL_HOSTS_KEY, null);
+        haClusterServer = jsonConfig.getString(HA_CLUSTER_SERVER_KEY, ClusterSettings.cluster_server.getDefaultValue());
+        haSlaveOnly = Boolean.valueOf(jsonConfig.getBoolean(HA_SLAVE_ONLY_KEY));
+        haServer = jsonConfig.getString(HA_SERVER_KEY, HaSettings.ha_server.getDefaultValue());
+        haServerId = jsonConfig.getString(HA_SERVER_ID_KEY, null);
     }
 
     public String getMode() {
@@ -74,7 +97,7 @@ public class Neo4VertxConfiguration {
      *
      * @return the cypher rest url.
      */
-
+    @JsonProperty(REST_URL_KEY)
     public String getRestUrl() {
         return restUrl;
     }
@@ -84,9 +107,9 @@ public class Neo4VertxConfiguration {
      *
      * @return base address prefix
      */
+    @JsonProperty(BASE_ADDR_KEY)
     public String getBaseAddress() {
         return baseAddress;
-
     }
 
     /**
@@ -94,6 +117,7 @@ public class Neo4VertxConfiguration {
      * 
      * @return webserver address
      */
+    @JsonProperty(WEB_SERVER_BIND_ADDRESS_KEY)
     public String getWebServerBindAddress() {
         return webServerBindAddress;
     }
@@ -103,6 +127,7 @@ public class Neo4VertxConfiguration {
      *
      * @return ha.initial_hosts property value
      */
+    @JsonProperty(HA_INITIAL_HOSTS_KEY)
     public String getHAInitialHosts() {
         return haInitialHosts;
     }
@@ -112,6 +137,7 @@ public class Neo4VertxConfiguration {
      *
      * @return ha.server_id property value
      */
+    @JsonProperty(HA_SERVER_ID_KEY)
     public String getHAServerID() {
         return haServerId;
     }
@@ -121,6 +147,7 @@ public class Neo4VertxConfiguration {
      *
      * @return ha.slave_only flag
      */
+    @JsonProperty(HA_SLAVE_ONLY_KEY)
     public boolean getHASlaveOnly() {
         return haSlaveOnly;
     }
@@ -130,6 +157,7 @@ public class Neo4VertxConfiguration {
      *
      * @return ha.server property value
      */
+    @JsonProperty(HA_SERVER_KEY)
     public String getHAServer() {
         return haServer;
     }
@@ -139,6 +167,7 @@ public class Neo4VertxConfiguration {
      *
      * @return ha.server_cluster property value
      */
+    @JsonProperty(HA_CLUSTER_SERVER_KEY)
     public String getHAClusterServer() {
         return haClusterServer;
     }
@@ -185,6 +214,27 @@ public class Neo4VertxConfiguration {
 
     public void setBaseAddress(String baseAddress) {
         this.baseAddress = baseAddress;
+    }
+
+    /**
+     * Wrap the configuration into a vertx json object.
+     * 
+     * @return
+     */
+    @JsonIgnore
+    public JsonObject getJsonConfig() {
+        JsonObject object = new JsonObject();
+        object.put(MODE_KEY, getMode());
+        object.put(PATH_KEY, getPath());
+        object.put(REST_URL_KEY, getRestUrl());
+        object.put(WEB_SERVER_BIND_ADDRESS_KEY, getWebServerBindAddress());
+        object.put(BASE_ADDR_KEY, getBaseAddress());
+        object.put(HA_INITIAL_HOSTS_KEY, getHAInitialHosts());
+        object.put(HA_CLUSTER_SERVER_KEY, getHAClusterServer());
+        object.put(HA_SLAVE_ONLY_KEY, getHASlaveOnly());
+        object.put(HA_SERVER_KEY, getHAServer());
+        object.put(HA_SERVER_ID_KEY, getHAServerID());
+        return object;
     }
 
 }
